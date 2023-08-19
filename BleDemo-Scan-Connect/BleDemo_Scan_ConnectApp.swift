251548e9d5978ca08ef3bff2c1057aa50edf3d0f
Gaurav Tak <gaurav.tak@roro.io>
2023-08-19 16:32:14 +0530

    Initial Commit

---
+++ BleDemo-Scan-Connect/BleDemo_Scan_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BleDemo_Scan_ConnectApp.swift
+//  BleDemo-Scan-Connect
+//
+//  Created by Roro Solutions LLP on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BleDemo_Scan_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
