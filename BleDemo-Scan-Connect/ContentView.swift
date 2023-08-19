42c3984ab6393c29232556b71c6f8b28df246bb6
Gaurav Tak <gaurav.tak@roro.io>
2023-08-19 20:25:31 +0530

    Detail view Text added and search added

--- BleDemo-Scan-Connect/ContentView.swift
+++ BleDemo-Scan-Connect/ContentView.swift
@@ -11,43 +11,73 @@ import CoreBluetooth
 struct ContentView: View {
     @ObservedObject private var bleScanner = BleScanner()
     @State private var searchText = ""
-
+    @State var showBleDeviceDetailsUi = false
+   
+    func buildView() -> some View {
+        return AnyView(Text("DetailView"))
+    }
     var body: some View {
-        VStack {
-            Text("List of Scanned Ble Devices").font(.title).padding(.top, 24)
-
-            // List of discovered peripherals filtered by search text
-            List(bleScanner.discoveredPeripheralDevices, id: \.peripheralDevice.identifier) { discoveredPeripheralDevice in
+        NavigationView {
+            VStack {
+                NavigationLink(
+                    destination: buildView(), isActive: $showBleDeviceDetailsUi
+                ) {
+                    EmptyView()
+                }.isDetailLink(false)
+                Text("List of Scanned Ble Devices").font(.title)
                 HStack {
-                    Text(discoveredPeripheralDevice.peripheralDevice.name ?? "Unknown Device").bold().frame(alignment: .leading)
-                    Spacer()
-                    Image("_next").frame(alignment: .trailing)
-                }.frame(maxWidth: .infinity)
-                    .onTapGesture {
-                        print("Ble Device Tapped")
-                      //  bleScanner.centralManager.connect(discoveredPeripheralDevice.peripheralDevice, options: nil)
+                    // Text field for entering search text
+                    TextField("Search", text: $searchText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    // Button for clearing search text
+                    Button(action: {
+                        self.searchText = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.secondary)
                     }
-            }.padding(.top, 16)
-
-            // Button for starting or stopping scanning
-            Button(action: {
-                if self.bleScanner.isScanningInProgress {
-                    self.bleScanner.stopScan()
-                } else {
-                    self.bleScanner.startScan()
+                    .buttonStyle(PlainButtonStyle())
+                    .opacity(searchText == "" ? 0 : 1)
                 }
-            }) {
-                if bleScanner.isScanningInProgress {
-                    Text("Stop Ble Scanning")
-                } else {
-                    Text("Scan for Ble Devices")
+                .padding()
+                // List of discovered peripherals filtered by search text
+                List(bleScanner.discoveredPeripheralDevices.filter {
+                    self.searchText.isEmpty ? true : $0.peripheralDevice.name?.lowercased().contains(self.searchText.lowercased()) == true
+                }, id: \.peripheralDevice.identifier) { discoveredPeripheralDevice in
+                    HStack {
+                        Text(discoveredPeripheralDevice.peripheralDevice.name ?? "Unknown Device").bold().frame(alignment: .leading)
+                        Spacer()
+                        Image("_next").frame(alignment: .trailing)
+                    }.frame(maxWidth: .infinity)
+                        .onTapGesture {
+                            print("Ble Device Tapped")
+                           // showBleDeviceDetailsUi = true
+                           // bleScanner.centralManager.connect(discoveredPeripheralDevice.peripheralDevice, options: nil)
+                        }
+                }.padding(.top, 16)
+                
+                // Button for starting or stopping scanning
+                Button(action: {
+                    showBleDeviceDetailsUi = true
+                    if self.bleScanner.isScanningInProgress {
+                        self.bleScanner.stopScan()
+                    } else {
+                        self.bleScanner.startScan()
+                    }
+                }) {
+                    if bleScanner.isScanningInProgress {
+                        Text("Stop Ble Scanning")
+                    } else {
+                        Text("Scan for Ble Devices")
+                    }
                 }
+                // Button looks cooler this way on iOS
+                .padding()
+                .background(bleScanner.isScanningInProgress ? Color.red : Color.blue)
+                .foregroundColor(Color.white)
+                .cornerRadius(5.0)
             }
-            // Button looks cooler this way on iOS
-            .padding()
-            .background(bleScanner.isScanningInProgress ? Color.red : Color.blue)
-            .foregroundColor(Color.white)
-            .cornerRadius(5.0)
-        }
+        }.ignoresSafeArea()
     }
 }
